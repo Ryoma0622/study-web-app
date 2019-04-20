8c2a2caf8986f50c63e3dca2443d11fdef2afff9
上土井涼馬 <sup0ry0jodoi@gmail.com>
2019-04-20 15:52:42 +0900

    spring initializr

---
+++ src/main/java/study/web/startApp/StartAppApplication.java
@@ -0,0 +1,13 @@
+package study.web.startApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StartAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StartAppApplication.class, args);
+	}
+
+}
